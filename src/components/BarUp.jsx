721528344afb40d4cfdb3a780bfe03471de2af30
Gabriel Galán <gabriel.galan92@gmail.com>
2021-07-23 17:28:50 -0500

    factory icons

--- src/components/BarUp.jsx
+++ src/components/BarUp.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { SvgXml } from 'react-native-svg';
-import { View, Text, TouchableOpacity } from 'react-native';
+//import { SvgXml } from 'react-native-svg';
+import { View, TouchableOpacity } from 'react-native';
 import styles from '../styles/styles';
-import userIcon  from './userIcon';
+//import userIcon  from './userIcon';
+//import backIcon from './backIcon';
+import Icons from './Icons';
 
 const BarUp = () => {
     return(
         <View style={styles.barUpContainer}>
             <TouchableOpacity>
+                <Icons icon={'backIcon'}/>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <SvgXml xml={userIcon} width="30" height="30" />
-            </TouchableOpacity>
+            
         </View>
     );
 }
---
+++ src/components/Icons.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SvgXml } from 'react-native-svg';
+import icons from '../utils/icons';
+import { View, Text } from 'react-native';
+
+
+const Icons = ({ icon, w = 30, h = 30 }) => {
+    if(icons.hasOwnProperty(icon)){
+        return <SvgXml xml={icons[icon]} width={w} height={h} /> ;
+    }
+    return (<View><Text> :( </Text></View>);
+};
+
+export default Icons;
