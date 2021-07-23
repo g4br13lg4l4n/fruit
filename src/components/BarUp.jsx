40854d4cb7c8d0592b7a517fce4a57375cbce3e8
Gabriel Galán <gabriel.galan92@gmail.com>
2021-07-23 17:29:09 -0500

    factory icons 2

--- src/components/BarUp.jsx
+++ src/components/BarUp.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-//import { SvgXml } from 'react-native-svg';
 import { View, TouchableOpacity } from 'react-native';
 import styles from '../styles/styles';
-//import userIcon  from './userIcon';
-//import backIcon from './backIcon';
 import Icons from './Icons';
 
 const BarUp = () => {
