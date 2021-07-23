29a61e1d89f433a22018d4bd049908946e2cee00
Gabriel Galán <gabriel.galan92@gmail.com>
2021-07-23 16:52:15 -0500

    icon

--- src/components/BarUp.jsx
+++ src/components/BarUp.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { SvgXml } from 'react-native-svg';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from '../styles/styles';
-import userIcon from './userIcon';
+import userIcon  from './userIcon';
 
 const BarUp = () => {
     return(
         <View style={styles.barUpContainer}>
-            <TouchableOpacity></TouchableOpacity>
+            <TouchableOpacity>
+            </TouchableOpacity>
             <TouchableOpacity>
                 <SvgXml xml={userIcon} width="30" height="30" />
             </TouchableOpacity>
